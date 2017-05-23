d7fdbc6c021138573743d70ee13fdcb2736ce5d3
ntja <jephte.ndongmou@gmail.com>
2017-05-23 14:14:18 +0200

    Debugging fromstoragecontroller bookks

--- v2/app/Http/Controllers/Books/FromStorageController.php
+++ v2/app/Http/Controllers/Books/FromStorageController.php
@@ -160,7 +160,7 @@ class FromStorageController extends Controller {
 			//$imagick->readImage($path);
 			//reduce the dimensions - scaling will lead to black color in transparent regions
 			$imagick->scaleImage(800,0);
-			$imagick->cropImage(500,500, 0,70);
+			$imagick->cropImage(100,100, 0,60);
 			//set new format
 			$imagick->setImageFormat('jpeg');
 			$imagick->setImageCompression(\Imagick::COMPRESSION_JPEG);
