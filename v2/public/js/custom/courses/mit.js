e032d68903efa9ee812719cc19b80fdb1fcfda32
jephte <jephte@sw.ai>
2017-09-27 20:25:13 +0100

    Updated mit.js script

--- v2/public/js/custom/courses/mit.js
+++ v2/public/js/custom/courses/mit.js
@@ -12,16 +12,18 @@
 		}else{
 			query = "q=computer science";
 		}
-		console.log(query);
+		//console.log(query);
 		
 		get_data(query);
 		
 		function get_data(q){
 			url =  "http://data.oeconsortium.org/api/v1/courses/search/?"+q;
-			console.log(url);
+			//console.log(url);			
 			$.get(url, function( data ) {
 			  //$( "#course_list" ).html( data.documents );
 			  var course_list = data.documents;
+			  $( "#course_list" ).empty();
+			  $('.pagination').empty();
 			  for(i=0;i<course_list.length;i++){
 				  //if(course_list[i].source == "Massachusetts Institute of Technology"){
 					$( "#course_list" ).append( '<p><a href="'+course_list[i].link+'" target="blank">'+course_list[i].title +'</a></p>' );
@@ -30,22 +32,25 @@
 			  console.info(data);
 			  //console.log(data.next_page);
 			  if(data.previous_page){
-					$('.pagination').append('<li><a href="javascript:void(0)" id="next">&laquo; Previous</a></li>');
+					$('.pagination').append('<li><a href="javascript:void(0)" data-previous="'+data.previous_page+'" id="previous">&laquo; Previous</a></li>');
 				}
 				if(data.next_page){			
-					$('.pagination').append('<li><a href="javascript:void(0)" id="previous"> Next &raquo; </a></li>');
+					$('.pagination').append('<li><a href="javascript:void(0)" data-next="'+data.next_page+'" id="next"> Next &raquo; </a></li>');
 				}			
-			});
-			//click on next page link
-			$('body').delegate('#next', 'click',function(data){				
-				get_data(data.next_page);
-			});
-			
-			//clcik on previous page link
-			$('body').delegate('#previous', 'click',function(data){
-				get_data(data.previous_page);
-			});
-		}								
+			});			
+		}
+		//click on next page link
+		$('body').delegate('#next', 'click',function(){
+			var page = $(this).data('next');
+			console.info(page);
+			get_data(page);
+		});
+		
+		//clcik on previous page link
+		$('body').delegate('#previous', 'click',function(){
+			var page = $(this).data('previous');
+			get_data(page);
+		});		
 		/**/								
 	});
 })(jQuery);
