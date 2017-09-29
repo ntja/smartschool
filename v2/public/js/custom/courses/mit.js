b507038b0698ec110d47d8937f35a775fd5b10ab
jephte <jephte@sw.ai>
2017-09-29 17:15:37 +0100

    Updated Project

--- v2/public/js/custom/courses/mit.js
+++ v2/public/js/custom/courses/mit.js
@@ -13,10 +13,11 @@
 			query = "q=computer science";
 		}
 		//console.log(query);
+		get_categories();
 		
-		get_data(query);
+		get_latest_course();
 		
-		function get_data(q){
+		function search(q){
 			url =  "http://data.oeconsortium.org/api/v1/courses/search/?"+q;
 			//console.log(url);			
 			$.get(url, function( data ) {
@@ -24,11 +25,16 @@
 			  var course_list = data.documents;
 			  $( "#course_list" ).empty();
 			  $('.pagination').empty();
+			  html = '<ul class="list_1">';
 			  for(i=0;i<course_list.length;i++){
 				  //if(course_list[i].source == "Massachusetts Institute of Technology"){
 					$( "#course_list" ).append( '<p><a href="'+course_list[i].link+'" target="blank">'+course_list[i].title +'</a></p>' );
+					html +=  '<li><a href="'+course_list[i].link+'" target="blank">'+course_list[i].title +'</a></li>' 
 				  //}			
+			  
 			  }
+			  html += '</ul>';
+			  $( "#course_list" ).html(html);
 			  console.info(data);
 			  //console.log(data.next_page);
 			  if(data.previous_page){
@@ -43,14 +49,82 @@
 		$('body').delegate('#next', 'click',function(){
 			var page = $(this).data('next');
 			console.info(page);
-			get_data(page);
+			get_course(page);
 		});
 		
 		//clcik on previous page link
 		$('body').delegate('#previous', 'click',function(){
 			var page = $(this).data('previous');
-			get_data(page);
+			get_course(page);
 		});		
-		/**/								
+		
+		//click on a category
+		$('body').delegate('.category', 'click',function(){
+			var category = $(this).data('id');
+			get_course(category);
+		});
+		
+		function get_categories(){
+			var url =  "http://data.oeconsortium.org/api/v1/categories/";
+			$.get(url, function( ) {			 
+			})
+			.done(function(data) {
+				console.log(data);
+				if(data.length>0){
+					//$('#category_list').empty();				
+					html = '';
+					for(i=0;i < data.length;i++){
+						html +='<li><a href="#" class="category" data-id="'+data[i].category_id+'">'+data[i].name+' ('+data[i].course_count+')</a></li>';
+					}
+					$('.subject').append(html);
+				}
+			})
+			  .fail(function() {
+				//alert( "error" );
+			})
+			  .always(function() {
+				//alert( "finished" );
+			});				
+		}
+		
+		function get_latest_course(){
+			url =  "http://data.oeconsortium.org/api/v1/courses/latest/";								
+			$.get(url, function( data ) {			  
+			  var course_list = data;
+			  $( "#course_list" ).empty();
+			  $('.pagination').empty();
+			  html = '<ul class="list_1">';
+			  for(i=0;i<data.length;i++){
+				html +=  '<li><a href="'+data[i].linkurl+'" target="blank">'+data[i].title +'</a></li>';
+			  }
+			  html += '</ul>';
+			  $( "#course_list" ).html(html);
+			  console.info(data);			  		
+			});			
+		}
+		
+		function get_course(cat){						
+			url =  "http://data.oeconsortium.org/api/v1/categories/"+cat+"/";			
+			//console.log(url);			
+			$.get(url, function( data ) {			  
+			  var course_list = data.results;
+			  $( "#course_list" ).empty();
+			  $('.pagination').empty();
+			  html = '<ul class="list_1">';
+			  for(i=0;i<course_list.length;i++){
+				html +=  '<li><a href="'+course_list[i].linkurl+'" target="blank">'+course_list[i].title +'</a></li>';
+			  }
+			  html += '</ul>';
+			  $( "#course_list" ).html(html);
+			  console.info(data);
+			  //console.log(data.next_page);
+			  if(data.previous){
+					$('.pagination').append('<li><a href="javascript:void(0)" data-previous="'+data.previous_page+'" id="previous">&laquo; Previous</a></li>');
+				}
+				if(data.next){			
+					$('.pagination').append('<li><a href="javascript:void(0)" data-next="'+data.next_page+'" id="next"> Next &raquo; </a></li>');
+				}			
+			});			
+		}
 	});
 })(jQuery);
--- v2/resources/views/ocw/catalog.blade.php
+++ v2/resources/views/ocw/catalog.blade.php
@@ -1,48 +1,32 @@
 @extends('layouts.master')
 
 @section('header-title')
-    SmartSchool | {{__('Search Results')}}
+    SmartSchool | {{__('Courses Catalog')}}
 @stop
 @section('header-styles')
-   <!-- CUSTOM STYLES -->   
-    <link href="{{asset('css/single_course.css')}}" rel="stylesheet">
+   <!-- CUSTOM STYLES -->
     <link href="{{asset('css/custom.css')}}" rel="stylesheet">
 @stop
-
 @section('content')
-<div class="notification-container"></div>
- @include('partials/header')
-  	
-<section id="sub-header">
-  	<div class="container">
-    
-    	<div class="row">
-        	<div class="col-md-12 text-center" id="course_title">
-                <p class="lead"></p>
+
+@include('partials/header')
+
+    <section id="sub-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 text-center">
+                <h1>{{__('OCW Courses Catalog')}}</h1>      
+                <p class="lead">
+                   {{__('Explore new interests and career opportunities with courses in Mathematics, Computer Science, Chemistry, Physics, Biology and more...')}}
+                </p>
             </div>
         </div><!-- End row -->
-        
-        <div class="row course_desc" id="sub-header-features-2">
-        	<div class="col-md-12 summary">
-            	<!--<h2>{{__('A brief summary')}}</h2>-->
-            </div> 
-            <div class="col-md-12">
-            	<!--
-                <h2>{{__('What you will learn')}}</h2>
-                <ul class="list_ok">
-                    <li><strong>Certified</strong> and expert teachers</li>
-                    <li><strong>Extensive</strong> doumentation provided</li>
-                    <li><strong>Money back</strong> garantee</li>
-                    <li><strong>Became an exeprt</strong> in only 6 days</li>
-                </ul>
-                -->
-           </div>
-        </div><!-- End row -->		
     </div><!-- End container -->
     <div class="divider_top"></div>
-  </section>
-  
-  <section id="main_content">
+    </section><!-- End sub-header -->
+    
+    
+    <section id="main_content">
     	<div class="container-fluid">
         
         <ol class="breadcrumb">
@@ -52,7 +36,7 @@
         
         <div class="row">
         
-        <aside class="col-lg-3 col-md-4 col-sm-3">
+        <aside class="col-lg-2 col-md-3 col-sm-2">
             <div class="box_style_">
             	<h4>{{__('Subjects')}}</h4>
 				<ul class="submenu-col subject">										
@@ -61,7 +45,7 @@
             </div>
         </aside>
         
-        <div class="col-lg-7 col-md-7 col-sm-9 col-lg-offset-1 col-md-offset-1">
+        <div class="col-lg-10 col-md-9 col-sm-10">
         	<div class="row" id="course_list">                                               
        		</div><!-- End row -->
         </div><!-- End col-lg-9-->                        
@@ -76,14 +60,14 @@
             	
         </div><!-- End container -->
     </section><!-- End main_content -->
- 
+
 @include('partials/footer')
 
 @section('scripts')
 	<script src="{{asset('js/custom/config/config.js')}}"></script>
 	<script src="{{asset('js/custom/functions.js')}}"></script>
-	<script src="{{asset('js/localization/i18n.js')}}"></script>	
-   <script src="{{asset('js/custom/courses/mit.js')}}"></script>
+	<script src="{{asset('js/localization/i18n.js')}}"></script>
+   <script src="{{asset('js/custom/ocw/catalog.js')}}"></script>
 @stop
 
 @stop 
