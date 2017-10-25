b00c203602dba2495455432a5230e93937661c65
jephte <jephte@sw.ai>
2017-10-25 14:05:58 +0100

    updated the header of the app

--- v2/resources/views/partials/header.blade.php
+++ v2/resources/views/partials/header.blade.php
@@ -66,8 +66,8 @@
 					-->
 				</li>				
 				<li><a href="<?php echo URL::to('/books/catalog'); ?>">{{__('Books')}}</a></li>
-				<li><a href="<?php echo URL::to('/ocw/courses'); ?>">{{__('OCW Courses')}}</a></li>
-				<li><a href="<?php echo URL::to('/questions'); ?>">{{__('Questions')}}</a></li>
+				<!-- <li><a href="<?php echo URL::to('/ocw/courses'); ?>">{{__('OCW Courses')}}</a></li> -->
+				<!-- <li><a href="<?php echo URL::to('/questions'); ?>">{{__('Questions')}}</a></li> -->
 			</ul>                       
 		</div>
 	</div><!-- End row -->
