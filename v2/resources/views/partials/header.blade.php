aa0d55777635963d003c6509479bf4d8816688a1
jephte <jephte.ndongmou@gmail.com>
2018-01-08 17:29:12 +0100

    reactivated OCW link for OCW courses

--- v2/resources/views/partials/header.blade.php
+++ v2/resources/views/partials/header.blade.php
@@ -65,7 +65,7 @@
 					-->
 				</li>				
 				<li><a href="<?php echo URL::to('/books/catalog'); ?>">{{__('Books')}}</a></li>
-				<!-- <li><a href="<?php echo URL::to('/ocw/courses'); ?>">{{__('OCW Courses')}}</a></li> -->
+				<!-- --><li><a href="<?php echo URL::to('/ocw/courses'); ?>">{{__('OCW Courses')}}</a></li> 
 				<!-- <li><a href="<?php echo URL::to('/questions'); ?>">{{__('Questions')}}</a></li> -->
 			</ul>                       
 		</div>
